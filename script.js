11a2313cecfe1a9e5bbbd9009d57ec758b934110
najmajinow <Najmajinow@gmail.com>
2025-06-13 11:43:30 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("contact-form").addEventListener("submit", function (event) {
-        event.preventDefault();
-        alert("Thank you for your message! We will get back to you soon.");
-        document.getElementById("contact-form").reset();
-    });
-});
+
